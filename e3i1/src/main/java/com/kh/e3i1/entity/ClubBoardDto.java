b5ebe32e76b4d19c3b674817ea6884f158b48a77
BeefJerky1 <hwb0404@naver.com>
2022-06-27 15:28:21 +0900

    beefjerky/1
    - 소모임 게시글 Dto
    - 소모임 게시글 첨부파일 Dto
    - 소모임 댓글 Dto

---
+++ e3i1/src/main/java/com/kh/e3i1/entity/ClubBoardDto.java
@@ -0,0 +1,21 @@
+package com.kh.e3i1.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Builder
+public class ClubBoardDto {
+	private int clubBoardNo;
+	private int clubNo;
+	private int clubBoardWriter;
+	private String clubBoardContent;
+	private Date clubBoardDate;
+	private int clubBoardCount;
+}
---
+++ e3i1/src/main/java/com/kh/e3i1/entity/ClubBoardReplyDto.java
@@ -0,0 +1,24 @@
+package com.kh.e3i1.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Builder
+public class ClubBoardReplyDto {
+	private int replyNo;
+	private int clubBoardNo;
+	private int clubNo;
+	private int clubReplyWriter;
+	private String clubReplyContent;
+	private Date clubReplyTime;
+	
+	
+	
+}
