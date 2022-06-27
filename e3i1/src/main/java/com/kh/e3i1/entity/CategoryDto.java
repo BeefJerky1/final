5bca7d6c0c01e774d39210261c7f16c584ac3918
JooDaEun <y0825131@naver.com>
2022-06-27 15:49:43 +0900

    categoryDto/MemberProfileDto 생성

---
+++ e3i1/src/main/java/com/kh/e3i1/entity/CategoryDto.java
@@ -0,0 +1,13 @@
+package com.kh.e3i1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class CategoryDto {
+	private int categoryNo;
+	private String categoryContent;
+	private int superNo;
+}
