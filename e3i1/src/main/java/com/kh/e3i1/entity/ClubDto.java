5995e3c57c9e803909f828c2ec91fb7077f3177f
sanholl <edgar.jang93@hanmail.net>
2022-06-27 15:25:37 +0900

    ClubDto / ClubMemberDto 생성

---
+++ e3i1/src/main/java/com/kh/e3i1/entity/ClubDto.java
@@ -0,0 +1,21 @@
+package com.kh.e3i1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class ClubDto {
+	private int clubNo;
+	private int clubLeader;
+	private String clubName;
+	private String clubSummary;
+	private String clubMainCategory;
+	private String clubSubCategory;
+	private String clubPlace;
+	private String clubJoinQuestioin1;
+	private String clubJoinQuestioin2;
+	private String clubJoinQuestioin3;
+	private int clubMemberLimit;
+}	
